8959292ade612618879e2d04574efe761109f2ba
odomaj <odomaj@clarkson.edu>
2023-10-22 20:37:08 -0400

    Update buildall.py

--- TicTacToeServer/scripts/buildall.py
+++ TicTacToeServer/scripts/buildall.py
@@ -21,9 +21,6 @@ os.system('javac -d ' + dir + ' ../src/server/ServerHandler.java ../src/server/S
 os.system('javac -d ' + dir + ' ../src/socket/Response.java ../src/socket/Request.java ../src/socket/Response.java')
 
 # build test package
-os.system('javac -d ' + dir + ' ../src/test/EventTest.java ../src/test/RequestTest.java ../src/test/ResponseTest.java ../src/test/SocketServerTest.java ../src/test/UserTest.java')
-
-# build main
-os.system('javac -d ' + dir + ' ../src/Main.java')
+os.system('javac -d ' + dir + ' ../src/test/EventTest.java ../src/test/RequestTest.java ../src/test/ResponseTest.java ../src/test/SocketServerTest.java ../src/test/UserTest.java ../src/test/Main.java')
 
 print('build complete')
