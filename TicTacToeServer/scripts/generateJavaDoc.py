7054bf3b8636b985e9514a0b69178bda3469e2e4
odomaj <odomaj@clarkson.edu>
2023-10-23 22:01:21 -0400

    Removed unused variable

--- TicTacToeServer/scripts/generateJavaDoc.py
+++ TicTacToeServer/scripts/generateJavaDoc.py
@@ -9,6 +9,4 @@ else:
     os.system('mkdir doc')
     os.chdir('doc')
 
-dir = os.getcwd()
-
 os.system('javadoc -author ../src/model/*.java ../src/server/*.java ../src/socket/*.java ../src/test/*.java')
