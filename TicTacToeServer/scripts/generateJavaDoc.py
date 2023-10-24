686dc9e03a293c89975d31f9e43fd6badefd560f
odomaj <odomaj@clarkson.edu>
2023-10-23 21:41:21 -0400

    Added script to generate javadoc (javadoc command must be added to PATH))

---
+++ TicTacToeServer/scripts/generateJavaDoc.py
@@ -0,0 +1,14 @@
+import os
+
+print('bulding project...')
+
+if(os.path.exists(os.getcwd() + '/../doc')):
+    os.chdir('../doc')
+else:
+    os.chdir('../')
+    os.system('mkdir doc')
+    os.chdir('doc')
+
+dir = os.getcwd()
+
+os.system('javadoc ../src/model/*.java ../src/server/*.java ../src/socket/*.java ../src/test/*.java')
