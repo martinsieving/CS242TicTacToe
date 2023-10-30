c60369c59433f143417daa0958dad796b4417599
odomaj <odomaj@clarkson.edu>
2023-10-30 11:25:42 -0400

    Added socket and username class variables to ServerHandler class

--- TicTacToeServer/src/server/ServerHandler.java
+++ TicTacToeServer/src/server/ServerHandler.java
@@ -7,11 +7,25 @@ package server;
  * Clarkson University CS 242, October 2023
  */
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.net.*;
+import javax.net.ssl.*;
+
 public class ServerHandler extends Thread
 {
-    ServerHandler()
-    {
+    private Socket socket;
+    private String currentUsername;
 
+    /**
+     * Constructor
+     * @param socket sets the socket class variable
+     * @param currentUsername sets the currentUsername class variable
+     */
+    ServerHandler(Socket socket, String currentUsername)
+    {
+        this.socket = socket;
+        this.currentUsername = currentUsername;
     }
 
     public void run()
