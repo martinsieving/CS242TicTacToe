12e59748e13636ae715740e6401dd8990810b6e3
odomaj <odomaj@clarkson.edu>
2023-12-10 20:35:34 -0500

    Removed broken version of Milestone 2

--- TicTacToeV4/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.1.1" apply false
-}
-
-dependencies {
-    // https://mvnrepository.com/artifact/com.google.code.gson/gson/2.10.1
-    implementation("com.google.code.gson:gson:2.10.1")
-}
